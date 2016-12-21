9136f1e91c71bd464aadf5f359576f89b2edcd78
gyasi <gyasi@10cc4532-ea35-4458-9172-d2e2002ae304>
2016-12-21 03:42:47 +0000

    Added KeyboardBounce

---
+++ KeyboardBounce/KeyboardBounce/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardBounce
+{
+    public partial class Form1 : Form
+    {
+        Bounce bouncer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bouncer = new Bounce();
+        }
+
+        private void TickTimer_Tick(object sender, EventArgs e)
+        {
+            bouncer.Tick();
+        }
+
+        private void StartStop_Click(object sender, EventArgs e)
+        {
+            if(TickTimer.Enabled)
+            {
+                TickTimer.Enabled = false;
+                StartStop.Text = "Start";
+            }
+            else
+            {
+                TickTimer.Enabled = true;
+                StartStop.Text = "Stop";
+            }
+        }
+    }
+}
